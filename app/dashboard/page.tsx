c277625caa032ee5cc5b333bf8ac14d4eb6105e0
bradapeter <silas_yao@yeah.net>
2025-03-17 08:23:22 +0800

    11

--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <p className=" min-h-screen  flex flex-col justify-center items-center gap-6 rounded-lg bg-gray-50 px-6 py-10 text-3xl">Dashboard Page</p>
-}
